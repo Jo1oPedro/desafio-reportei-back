8556bb26b127bba1b75435171aed2a46bc6d71ca
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-04 17:47:27 -0300

    Adiciona envio da dependecia de paginação para o serviço de do github

--- app/app/Http/Controllers/Api/GithubRepositoryController.php
+++ app/app/Http/Controllers/Api/GithubRepositoryController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 use App\Services\GithubService;
-use Illuminate\Support\Facades\Http;
+use App\Services\Pagination;
+use Illuminate\Http\Request;
 
 class GithubRepositoryController
 {
@@ -10,9 +11,12 @@ class GithubRepositoryController
         private GithubService $githubService
     ) {}
 
-    public function index()
+    public function index(Request $request)
     {
-        $user = auth()->user();
-        return $this->githubService->getUserRepositories($user->access_token);
+        $pagination = new Pagination(
+            $request->input('page', 1),
+            $request->input('per_page', 10)
+        );
+        return $this->githubService->getUserRepositories($pagination);
     }
 }
