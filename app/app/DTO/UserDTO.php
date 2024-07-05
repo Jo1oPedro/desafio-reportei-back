bcffca70acdb8ae1ae086e1834ea20705c868b81
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-05 15:28:11 -0300

    Adiciona campo de login do usuario, altera propriedades para privado e adiciona métodos get

--- app/app/DTO/UserDTO.php
+++ app/app/DTO/UserDTO.php
@@ -5,11 +5,40 @@ namespace App\DTO;
 class UserDTO
 {
     public function __construct(
-        public string $name,
-        public string $email,
-        public string $avatar_url,
-        public string $html_url,
-        public string $user_id,
-        public string $access_token
+        private readonly string $name,
+        private readonly string $email,
+        private readonly string $avatar_url,
+        private readonly string $github_login,
+        private readonly string $github_id,
+        private readonly string $access_token
     ) {}
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+    public function getAvatarUrl(): string
+    {
+        return $this->avatar_url;
+    }
+
+    public function getGithubLogin(): string
+    {
+        return $this->github_login;
+    }
+
+    public function getGithubId(): string
+    {
+        return $this->github_id;
+    }
+
+    public function getAccessToken(): string
+    {
+        return $this->access_token;
+    }
 }
