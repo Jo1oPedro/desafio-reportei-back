976f7803039141a4c2d81361f841760e2634e7d4
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-05 15:27:24 -0300

    Adiciona login e corrige nome do campo id do github na tabela de users

--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('user_id')->unique();
+            $table->string('github_login');
+            $table->string('github_id')->unique();
             $table->string('avatar_url');
-            $table->string('html_url');
             $table->string("access_token")->unique();
             $table->string('password')->nullable();
             $table->timestamp('email_verified_at')->nullable();
