0b1c8b4fccad85bba9d43190863ebc4bac2a41c1
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-05 23:05:26 -0300

    Adiciona retorno dos repositorios junt oao total e opção de cache

--- app/app/Services/GithubService.php
+++ app/app/Services/GithubService.php
@@ -4,22 +4,36 @@ namespace App\Services;
 
 use App\DTO\PaginationDTO;
 use App\DTO\RepositoryDTO;
+use App\Helper\RepositoryCommitsAnalyzer;
 use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Support\Facades\Http;
 
 class GithubService
 {
     public function __construct(
-        private RepositoryService $repository_service
+        private RepositoryService $repository_service,
+        private RepositoryCommitsAnalyzer $repository_commits_analyzer,
+        private CacheService $cache_service,
     ) {}
 
-    public function getUserRepositories(PaginationDTO $pagination)
+    public function getUserRepositories(PaginationDTO $pagination, bool $cached = true)
     {
+        if($cached) {
+            $total_items = $this->cache_service->remember("totalRepositories", function () {
+                return $this->getTotalRepositories();
+            });
+        } else {
+            $total_items = $this->getTotalRepositories();
+        }
+
         $response = $this->createHttpClient()
             ->get("https://api.github.com/user/repos?page={$pagination->getPage()}&per_page={$pagination->getPerPage()}&affiliation=owner");
 
         if($response->successful()) {
-            return response()->json($response->json());
+            return response()->json([
+                "repositories" => $response->json(),
+                "total_repositories" => $total_items
+            ]);
         } else {
             return response()->json([
                 "message" => "Failed to fetch repositories from Github"
@@ -29,10 +43,13 @@ class GithubService
 
     public function getRepository(RepositoryDTO $repositoryDTO)
     {
-        $this->repository_service->create($repositoryDTO);
-        $response = $this->createHttpClient()
-            ->get("https://api.github.com/repos/{$repositoryDTO->getOwnerName()}/{$repositoryDTO->getName()}/activity");
-
+        /** @todo adicionar cache com redis pro repositoryid */
+        $repository = $this->repository_service->create($repositoryDTO);
+        //if($repository->wasRecentlyCreated) {
+            $response = $this->createHttpClient()
+                ->get("https://api.github.com/repos/{$repositoryDTO->getOwnerName()}/{$repositoryDTO->getName()}/commits");
+            //return $this->repository_commits_analyzer->analyzeRepositoryCommits($response->json());
+        //}
         if($response->successful()) {
             return response()->json($response->json());
         } else {
@@ -49,5 +66,20 @@ class GithubService
             "Authorization" => "Bearer {$access_token}",
             "Accept" => "application/json"
         ]);
+    }
+
+    private function getTotalRepositories()
+    {
+        $response = $this->createHttpClient()
+            ->get("https://api.github.com/user/repos?page=1&per_page=1&affiliation=owner", [
+                "page" => 1,
+                "per_page" => 1,
+                "affiliation" => "owner"
+            ]);
+        $link = $response->headers()["Link"][0];
+        preg_match('/<([^>]*)>; rel="last"/', $link, $matches);
+        $last_url = $matches[1];
+        preg_match('/page=(\d+)/', $last_url, $pageMatches);
+        return $pageMatches[1];
     }
 }
