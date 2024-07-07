b89b0a1db8c252b3dea650e7e64baef729ce9796
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-07 15:12:02 -0300

    Adiciona analizador de commits do github

---
+++ app/app/Services/RepositoryCommitsAnalyzerService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Services;
+
+use Carbon\Carbon;
+use Laravel\Octane\Facades\Octane;
+
+class RepositoryCommitsAnalyzerService
+{
+    public function analyzeRepositoryCommits(array ...$jsons)
+    {
+        $tasks = [];
+        foreach ($jsons as $json) {
+            $tasks[] = fn() => $this->getTotalCommitsForEachDay($json);
+        }
+
+        $results = Octane::concurrently($tasks);
+
+        $total_commit_per_day = [];
+        foreach($results as $result) {
+            foreach($result as $date => $total_commits) {
+                if(array_key_exists($date, $total_commit_per_day)) {
+                    $total_commit_per_day[$date] += $total_commits;
+                    continue;
+                }
+                $total_commit_per_day[$date] = $total_commits;
+            }
+        }
+        return $total_commit_per_day;
+    }
+
+    public function getTotalCommitsForEachDay(array $json)
+    {
+        $totalCommits = [];
+        foreach($json as $data) {
+            $commit_date = Carbon::parse($data["commit"]["committer"]["date"])
+                ->format("dmy");
+            if(array_key_exists($commit_date, $totalCommits)) {
+                $totalCommits[$commit_date] += 1;
+                continue;
+            }
+            $totalCommits[$commit_date] = 1;
+        }
+        return $totalCommits;
+    }
+
+}
