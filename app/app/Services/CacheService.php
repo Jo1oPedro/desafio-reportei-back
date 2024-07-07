122dd297db23169c73af6a5dae45b77a44cfa7c7
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-06 21:44:10 -0300

    Adiciona métodos de put e forget ao cache

--- app/app/Services/CacheService.php
+++ app/app/Services/CacheService.php
@@ -6,8 +6,19 @@ use Illuminate\Support\Facades\Cache;
 
 class CacheService
 {
-    public function remember(string $key, callable $callback, int $seconds = 86400)
+    public function put($key, $value, $seconds = 86400): mixed
+    {
+        return Cache::put($key, $value, $seconds);
+    }
+
+    public function remember(string $key, callable $callback, int $seconds = 86400): mixed
     {
         return Cache::remember($key, $seconds, $callback);
+    }
+
+    public function forget($key): CacheService
+    {
+        Cache::forget($key);
+        return $this;
     }
 }
