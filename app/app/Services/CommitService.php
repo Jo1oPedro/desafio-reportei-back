986b9b5b4bc3c8016fc07a1878ea4b4e58c6b094
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-08 01:48:40 -0300

    Adiciona CommitService para abstrair logica de inserção de commits

---
+++ app/app/Services/CommitService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+use App\DTO\CommitDTO;
+use App\Models\Commit;
+
+class CommitService
+{
+    public function create(CommitDTO $commitDTO): Commit
+    {
+        return Commit::firstOrCreate(
+            ["commit_id" => $commitDTO->getCommitId()],
+            [
+                "author_login" => $commitDTO->getAuthorLogin(),
+                "author_id" => $commitDTO->getAuthorId(),
+                "commited_at" => $commitDTO->getCommitedAt()
+            ]
+        );
+    }
+}
