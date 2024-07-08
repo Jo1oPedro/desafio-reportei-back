d26ee83ae82d370a2205f314c54b4c76b48d1543
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-08 03:06:21 -0300

    Adiciona métodos has e get ao proxy de cache

--- app/app/Services/CacheService.php
+++ app/app/Services/CacheService.php
@@ -6,7 +6,11 @@ use Illuminate\Support\Facades\Cache;
 
 class CacheService
 {
-    public function put($key, $value, $seconds = 86400): mixed
+    public function get(string $key): mixed
+    {
+        return Cache::get($key);
+    }
+    public function put(string $key, mixed $value, int $seconds = 86400): mixed
     {
         return Cache::put($key, $value, $seconds);
     }
@@ -16,9 +20,14 @@ class CacheService
         return Cache::remember($key, $seconds, $callback);
     }
 
-    public function forget($key): CacheService
+    public function forget(string $key): CacheService
     {
         Cache::forget($key);
         return $this;
+    }
+
+    public function has(string $key): bool
+    {
+        return Cache::has($key);
     }
 }
