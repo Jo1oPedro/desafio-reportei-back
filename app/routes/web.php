9554ff97cdc6e54dd74c974302a0cb49565180b9
João Pedro <joao.pedreira@estudante.ufjf.br>
2024-07-08 21:53:59 -0300

    Retira rotas inutilizadas do arquivo web

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,10 +1,5 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Jobs\VisitaJob;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,36 +10,3 @@ use Illuminate\Support\Facades\Storage;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', function () {
-    $key = "redis-welcome-views";
-    $views = null;
-    try {
-        $redis = Redis::connection('default');
-        $redis->incr($key, 1);
-        $views = $redis->get($key, null);
-    } catch (\Throwable $th) {
-    }
-
-    VisitaJob::dispatch(request()->ip())->delay(now()->addSeconds(15))->onQueue('high');
-
-    return view('welcome')->with('views', $views);
-});
-
-Route::get('/documentation', function () {
-    $filePath = public_path('api-documentation/index.html'); // Replace with your actual file path
-
-    if (!Storage::disk('local')->exists($filePath)) {
-        abort(404, 'File not found'); // Handle non-existent files gracefully
-    }
-
-    $fileContents = Storage::disk('local')->get($filePath);
-    $mimeType = 'text/html'; // Set the MIME type explicitly for HTML
-
-    $headers = [
-        'Content-Type' => $mimeType,
-        'Content-Disposition' => 'attachment; filename="api-documentation.html"', // Set attachment disposition for saving
-    ];
-
-    return response($fileContents, 200, $headers);
-});
